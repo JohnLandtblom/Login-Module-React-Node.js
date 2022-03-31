a4cb30d5542da9456a2de36951d755c491d4a590
John Landtblom <john.landtblom@live.se>
2022-03-31 19:55:38 +0200

    final commit

--- src/GetUser.js
+++ src/GetUser.js
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
 import UserLogout from "./UserLogout";
 import "./GetUser.css";
 
 const GetUser = () => {
   const [data, setData] = useState(null);
+
+  /*useEffect(() => {
+    Axios({
+      method: "GET",
+      withCredentials: true,
+      url: "http://localhost:4000/user",
+    }).then((res) => {
+      setData(res.data);
+      console.log(res.data);
+    }, []);
+  });*/
   const getUser = () => {
     Axios({
       method: "GET",
